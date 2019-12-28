e7a311cd71385f3a5917eddc3278cdb08c48cb81
slahiff <sean243446@gmail.com>
2019-12-28 15:37:08 -0500

    Generated migration to connect resources
    1. Generated migration to connect user to responsibilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_28_203154) do
+ActiveRecord::Schema.define(version: 2019_12_28_203615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_12_28_203154) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_responsibilities_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema.define(version: 2019_12_28_203154) do
 
   add_foreign_key "examples", "users"
   add_foreign_key "projects", "users"
+  add_foreign_key "responsibilities", "users"
 end
